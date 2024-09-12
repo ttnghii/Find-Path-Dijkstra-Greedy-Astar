457ea5893040d103686fd4c8cb253bd56fdfac04
ttnghii <takoyaki1402@gmail.com>
2024-09-13 03:37:53 +0700

    build greedy function

---
+++ greedy.py
@@ -0,0 +1,22 @@
+from collections import list 
+
+def greedy(nodes: list, edges: list, start: list) -> tuple:
+    '''
+    Build the function using Greedy algorithm to find the
+    shortest path over all nodes from start node.
+    '''
+    current_node = start
+    visited = [start]
+    unvisited = [node[2] for node in nodes if node[2] != start]
+    total_weight = 0
+
+    while unvisited:
+        nearest_neighbor = min(
+            unvisited,
+            key=lambda x: edges[current_node, x]
+        )
+        total_weight += edges[current_node, nearest_neighbor]
+
+        current_node = nearest_neighbor
+        unvisited.remove(nearest_neighbor)
+        visited.append(nearest_neighbor)
