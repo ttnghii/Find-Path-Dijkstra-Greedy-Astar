aa091f936668b651fde0abfb846055660c533b76
ttnghii <takoyaki1402@gmail.com>
2024-09-13 03:48:41 +0700

    heuristic

--- build_graph.py
+++ build_graph.py
@@ -1,4 +1,4 @@
-from collections import list, defaultdict
+from collections import list, defaultdict, dict
 from math import sqrt
 
 
@@ -24,20 +24,35 @@ def name_coords(coords: list) -> list:
     return coords
 
 
-def graph(coords: list) -> tuple:
+def graph(coords: list, algorithm: str = None):
     '''
     Create weighted and undirected graph
-    Returns named coordinates and their connected edges as a dictionary
+    Returns:
+        - Named coordinates and their connected edges as a dictionary for
+        Dijkstra and Greedy algorithm.
+        - All of them and the graph for Astar algorithm.
     '''
-    coords = name_coords(coords=coords)
-    # Because there is small graphs, so we use adjacency matrix
-    adjacency_edge = defaultdict(list)
+    coords = name_coords(coords)
+    graphs = defaultdict(list)
+    edges = dict
 
     for current in coords:
         for comparer in coords:
             if comparer == current:
                 continue
-            weight = distance_between_coords(current[0], current[1], comparer[0], comparer[1])
-            adjacency_edge[current[2]].append((comparer[2], weight))
+            else:
+                weight = distance_between_coords(current[0], current[1],
+                                                 comparer[0], comparer[1])
+                graphs[current[2]].append(comparer[2]) 
+                edges[current[2], comparer[2]] = weight 
+    
+    if algorithm == 'Astar':
+        return coords, graphs, edges
+    else:
+        return coords, edges
+    
 
-    return coords, adjacency_edge
+def heuristic(node1: list, node2: list) -> float:
+    x1, y1 = node1[:2]
+    x2, y2 = node2[:2]
+    return distance_between_coords(x1, x2, y1, y2)
